c62f743b9fcbb44ef2b21cd80540fc6913820211
ndamatta <damatta.natanael@gmail.com>
2024-02-29 12:03:44 -0300

    fix addcart bug

--- src/js/product.js
+++ src/js/product.js
@@ -1,18 +1,24 @@
 import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let cartItems = getLocalStorage("so-cart");
+  if (!Array.isArray(cartItems)) {
+    cartItems = [];
+  }
+  // Push product and set local storage
+  cartItems.push(product);
+  setLocalStorage("so-cart", cartItems);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
   addProductToCart(product);
 }
-
-// add listener to Add to Cart button
+// add listener to AddToCart btn
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);

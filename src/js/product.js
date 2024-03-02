f38065cb52bd398953ece189124482c381f19999
coralin <zxc188950@yahoo.com.tw>
2024-03-02 21:24:46 +0800

    lcy-Add Item to Cart Broken

--- src/js/product.js
+++ src/js/product.js
@@ -1,15 +1,24 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let storedItems = getLocalStorage("so-cart");
+  let newStoredItems = [];
+  if (storedItems !== null) {
+    newStoredItems = storedItems;
+  }
+  newStoredItems.push(product);
+  // console.log(newStoredItems)
+  setLocalStorage("so-cart", newStoredItems);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
   addProductToCart(product);
+  //remove all item for testing
+  // localStorage.removeItem("so-cart");
 }
 
 // add listener to Add to Cart button

5ac417aac9764aa32e2b4bd63c93ded0a9df9b8e
wilsonBYU <wilsonomar@byui.edu>
2024-03-11 17:25:54 -0600

    Fixed format

--- src/js/alert.mjs
+++ src/js/alert.mjs
@@ -1,53 +1,52 @@
 //Creates the alert template with javascript for better performance and event handling
 const singleAlertTemplate = (alertData, selfClose) => {
-  //Alert container 
-  const alertContainer = document.createElement("section")
-  alertContainer.classList.add("alert", "visible", alertData.type)
+  //Alert container
+  const alertContainer = document.createElement("section");
+  alertContainer.classList.add("alert", "visible", alertData.type);
 
   //Text nodes
-  const alertText = document.createElement("div")
-  alertText.classList.add("alert-text")
-  const title = document.createElement("h4")
-  title.classList.add("alert-text__title")
-  title.innerText = alertData.title
-  const body = document.createElement("p")
-  body.classList.add("alert-text__content")
-  body.innerText = alertData.body
-  alertText.append(title, body)
+  const alertText = document.createElement("div");
+  alertText.classList.add("alert-text");
+  const title = document.createElement("h4");
+  title.classList.add("alert-text__title");
+  title.innerText = alertData.title;
+  const body = document.createElement("p");
+  body.classList.add("alert-text__content");
+  body.innerText = alertData.body;
+  alertText.append(title, body);
 
   //action nodes
-  const alertActions = document.createElement("div")
-  const button = document.createElement("button")
-  button.classList.add("alert-action__close")
-  button.innerText = "x"
+  const alertActions = document.createElement("div");
+  const button = document.createElement("button");
+  button.classList.add("alert-action__close");
+  button.innerText = "x";
   button.addEventListener("click", () => {
-    alertContainer.classList.toggle("visible")
-    alertContainer.classList.add("delete")
+    alertContainer.classList.toggle("visible");
+    alertContainer.classList.add("delete");
     alertContainer.addEventListener("animationend", () => {
-      alertContainer.remove()
-    })
-  })
-  alertActions.append(button)
+      alertContainer.remove();
+    });
+  });
+  alertActions.append(button);
 
-  alertContainer.append(alertText, alertActions)
+  alertContainer.append(alertText, alertActions);
 
-  return alertContainer
-
-}
+  return alertContainer;
+};
 
 //Alert class
 export default class Alert {
   constructor() {
-    this.parentElement = null
+    this.parentElement = null;
   }
 
   //for now we only need to check if the alert-list container exists if not we create it
   init() {
-    this.setParentAlertElement()
+    this.setParentAlertElement();
   }
 
   alertsDemo(dataSource) {
-    this.renderAlerts(dataSource)
+    this.renderAlerts(dataSource);
   }
 
   //this function will close the alert in 10 seconds if we set autoClose to true
@@ -55,44 +54,44 @@ export default class Alert {
     alertContainer.addEventListener("animationend", (e) => {
       if (e.target.classList.contains("visible")) {
         setTimeout(() => {
-          e.target.classList.toggle("visible")
-          e.target.classList.add("delete")
+          e.target.classList.toggle("visible");
+          e.target.classList.add("delete");
         }, 10000);
       } else if (e.target.classList.contains("delete")) {
-        e.target.remove()
+        e.target.remove();
       }
-    })
-    return alertContainer
+    });
+    return alertContainer;
   }
 
   //Check if the parent element exists if not, creates it and append to the body
   setParentAlertElement() {
-    const alertListParentElement = document.querySelector(".alert-list")
+    const alertListParentElement = document.querySelector(".alert-list");
     if (!alertListParentElement) {
-      let parent = document.createElement("section")
-      parent.classList.add("alert-list")
-      this.parentElement = parent
-      document.querySelector("body").prepend(parent)
+      let parent = document.createElement("section");
+      parent.classList.add("alert-list");
+      this.parentElement = parent;
+      document.querySelector("body").prepend(parent);
     } else {
-      this.parentElement = alertListParentElement
+      this.parentElement = alertListParentElement;
     }
   }
 
   //this will create a new alert of any type
   renderAlert(title = "", text = "", type = "primary") {
-    let alert = singleAlertTemplate({ "title": title, "body": text, "type": type })
-    let alertWithAutoClose = this.removeAlertHandler(alert)
-    this.parentElement.append(alertWithAutoClose)
-
+    let alert = singleAlertTemplate({ title: title, body: text, type: type });
+    let alertWithAutoClose = this.removeAlertHandler(alert);
+    this.parentElement.append(alertWithAutoClose);
   }
 
   //
   renderAlerts(alertList) {
-    let alerts = alertList.map(alertData => singleAlertTemplate(alertData, this.autoClose))
-    alerts.map(alert => {
-      let alertWithAutoClose = this.removeAlertHandler(alert)
-      this.parentElement.appendChild(alertWithAutoClose)
-    })
+    let alerts = alertList.map((alertData) =>
+      singleAlertTemplate(alertData, this.autoClose),
+    );
+    alerts.map((alert) => {
+      let alertWithAutoClose = this.removeAlertHandler(alert);
+      this.parentElement.appendChild(alertWithAutoClose);
+    });
   }
-
 }
--- src/js/main.js
+++ src/js/main.js
@@ -2,14 +2,11 @@ import ProductData from "./ProductData.mjs";
 import ProductListing from "./ProductList.mjs";
 import Alert from "./alert.mjs";
 
-
 const dataSource = new ProductData("tents");
 const productList = document.querySelector(".product-list");
 
 const tentList = new ProductListing("tents", dataSource, productList);
 tentList.init();
-
-
 
 /*
 ALERT FEATURE
@@ -33,15 +30,19 @@ the alert types are:
             danger danger (pink)
 */
 
-const dataAlertSource = new ProductData("alerts")
-const alert = new Alert()
-alert.init()
+const dataAlertSource = new ProductData("alerts");
+const alert = new Alert();
+alert.init();
 
 //As the assignment requirement, we are getting the alert data from a json file
 const renderDemo = async () => {
-  let data = await dataAlertSource.getData()
-  alert.alertsDemo(data)
-}
-alert.renderAlert("Promo Code!", "Use this promocode: #5234234123 to get a 10% off on tents!", "success")
+  let data = await dataAlertSource.getData();
+  alert.alertsDemo(data);
+};
+alert.renderAlert(
+  "Promo Code!",
+  "Use this promocode: #5234234123 to get a 10% off on tents!",
+  "success",
+);
 
-renderDemo()
+renderDemo();

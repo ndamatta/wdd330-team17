600488945dbea5beac15a6280259d9db9250feaf
coralin <zxc188950@yahoo.com.tw>
2024-03-21 11:42:21 +0800

    update task product-list-sort

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -19,6 +19,7 @@ export default class ProductListing {
     this.category = category;
     this.dataSource = dataSource;
     this.listElement = listElement;
+    this.sortType = "";
   }
 
   async init() {
@@ -26,9 +27,36 @@ export default class ProductListing {
     document.querySelector("title").innerText = `Top products: ${category}`;
     const list = await this.dataSource.getData(this.category);
     this.renderList(list);
+    this.sortTypeListener(list);
+    // this.sortBySelectedType(list);
   }
 
   renderList(list) {
     renderListWithTemplate(productCartTemplate, this.listElement, list);
   }
+  sortTypeListener(list) {
+    const sortButtons = document.querySelectorAll("input[name='sorttype']");
+    sortButtons.forEach((btn) => {
+      btn.addEventListener("change", (e) => {
+        this.sortType = e.target.value;
+        this.renderListBySort(list);
+      });
+    });
+  }
+  renderListBySort(list) {
+    if (this.sortType === "Name") {
+      list.sort((a, b) => a.Name.localeCompare(b.Name));
+    } else if (this.sortType === "Price") {
+      list.sort((a, b) => a.FinalPrice - b.FinalPrice);
+    }
+    renderListWithTemplate(
+      productCartTemplate,
+      this.listElement,
+      list,
+      "afterbegin",
+      true,
+    );
+  }
+
+  sortList;
 }
